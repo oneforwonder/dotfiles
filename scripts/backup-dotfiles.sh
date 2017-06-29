4d5b3125220f049bd409db82eec56b9560b137c0
Rob Nagle <rob@oneforwonder.com>
2017-06-28 22:53:05 -0700

    atom: add atom config files

--- scripts/backup-dotfiles.sh
+++ scripts/backup-dotfiles.sh
@@ -8,6 +8,11 @@ cp ~/.bash_profile ~/.dotfiles/backup/
 cp ~/.bash_logout ~/.dotfiles/backup/
 cp ~/.vimrc ~/.dotfiles/backup/
 cp -R ~/.vim ~/.dotfiles/backup/
+cp ~/.atom/config.cson ~/.dotfiles/backup
+cp ~/.atom/init.coffee ~/.dotfiles/backup
+cp ~/.atom/keymap.cson ~/.dotfiles/backup
+cp ~/.atom/snippets.cson ~/.dotfiles/backup
+cp ~/.atom/styles.less ~/.dotfiles/backup
 cp ~/.tmux.conf ~/.dotfiles/backup/
 cp ~/.gitconfig ~/.dotfiles/backup/
 cp ~/.gitignore_global ~/.dotfiles/backup/
